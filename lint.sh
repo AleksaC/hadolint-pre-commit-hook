8ca25c0e86fca5e35e04bf364d27b9ab91d7d7c1
AleksaC <aleksacukovic1@gmail.com>
2020-05-13 22:25:25 +0200

    Add MacOS support

--- lint.sh
+++ lint.sh
@@ -4,9 +4,19 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 HADOLINT="${DIR}/hadolint"
 
+BASE_URL="https://github.com/hadolint/hadolint/releases/download/v1.17.6"
+case $OSTYPE in
+    "linux-gnu")
+        URL="${BASE_URL}/hadolint-Linux-x86_64"
+        ;;
+    "darwin")
+        URL="${BASE_URL}/hadolint-Darwin-x86_64"
+        ;;
+esac
+
 if [ ! -f $HADOLINT ]; then
     echo -e "\nDownloading hadolint...\n"
-    wget -O $HADOLINT https://github.com/hadolint/hadolint/releases/download/v1.17.6/hadolint-Linux-x86_64 &> /dev/null
+    wget -O $HADOLINT $URL &> /dev/null
 fi
 
 if [ ! -x ./hadolint ]; then
